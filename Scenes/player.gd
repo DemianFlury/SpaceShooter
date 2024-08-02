7b2c46434f1bcd42434b5b529c861f8818380f61
DemianFlury <demianflury@proton.me>
2024-08-02 13:43:44 +0200

    add collisions

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends CharacterBody2D
 
 var speed := 500
 
@@ -10,4 +10,5 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var direction = Input.get_vector("left", "right", "forward", "back")
-	position += direction * speed * delta
+	velocity = direction * speed
+	move_and_slide()
