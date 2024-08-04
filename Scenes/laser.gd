358fd304bb8759045f22cdd0251e06b738ce2f2c
DemianFlury <demianflury@proton.me>
2024-08-04 21:36:03 +0200

    add charge up animation when shooting

--- Scenes/laser.gd
+++ Scenes/laser.gd
@@ -2,5 +2,9 @@ extends Area2D
 
 @export var bullet_speed = 500
 
+func _ready():
+	var tween = create_tween()
+	tween.tween_property($LaserSprite, 'scale', Vector2(1,1), 0.1).from(Vector2(0.3,0.3))
+
 func _process(delta):
 	position.y -= bullet_speed * delta
--- Scenes/player.gd
+++ Scenes/player.gd
@@ -3,23 +3,36 @@ extends CharacterBody2D
 var speed := 500
 signal shoot_laser(pos)
 var gun_ready := true
+var shot_fired := false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$LaserGunAnimation.visible = false
+	$LaserGunAnimation.frame = 0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+	# movement
 	var direction = Input.get_vector("left", "right", "forward", "back")
 	velocity = direction * speed
 	move_and_slide()
 	
 	if Input.is_action_just_pressed("shoot") and gun_ready:
-		shoot_laser.emit($LaserGun.global_position)
-		gun_ready = false
-		$LaserGunTimer.start()
+		# laser gun was fired, start animation
+		shot_fired = true
+		$LaserGunAnimation.visible = true
+		$LaserGunAnimation.play()
 
 
 func _on_laser_gun_timer_timeout():
 	gun_ready = true
+
+
+func _on_laser_gun_animation_animation_finished():
+	# firing aniomation is done, spawn the laser
+	$LaserGunAnimation.visible = false
+	shoot_laser.emit($LaserGun.global_position)
+	gun_ready = false
+	shot_fired = false
+	$LaserGunTimer.start()
