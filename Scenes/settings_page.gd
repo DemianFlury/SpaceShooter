eb50a9c707e006463faeec5513bc8f38dca1bedb
DemianFlury <demianflury@proton.me>
2024-11-05 19:34:59 +0100

    add buttons to settings page

---
+++ Scenes/settings_page.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+func _on_button_skin_button_down():
+	print('skin')
+
+func _on_button_audio_button_down():
+	print('audio')
+
+func _on_button_keybinds_button_down():
+	print('keybinds')
